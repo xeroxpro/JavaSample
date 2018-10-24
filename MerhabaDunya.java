e7e89cb1e168f54c485f65ec5b2dc8f394a5c3cb
xeroxpro <xeroxpro@gmail.com>
2018-10-24 13:24:11 +0300

    İlk Yukleme

---
+++ MerhabaDunya.java
@@ -0,0 +1,6 @@
+public class MerhabaDunya {
+   public static void main(String[] args) {
+       // Prints "Merhaba Dünya" in the terminal window.
+       System.out.println("Merhaba, Dünya");
+   }
+}
